c95d44e675c357aed6a82a91045524c225659c40
NSViking <victorvicente19@gmail.com>
2018-10-20 13:15:50 +0200

    Created HTTPClient

---
+++ Random/Random/Clients/HTTPClient/HTTPClient.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPClient.swift
+//  Random
+//
+//  Created by Víctor Vicente on 20/10/2018.
+//  Copyright © 2018 AKA7. All rights reserved.
+//
+
+import Foundation
+
+class HTTPClient: HTTPClientContract {
+    
+    func get(url: String) {
+        
+    }
+}
---
+++ Random/Random/Clients/HTTPClient/HTTPClientContract.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPClientContract.swift
+//  Random
+//
+//  Created by Víctor Vicente on 20/10/2018.
+//  Copyright © 2018 AKA7. All rights reserved.
+//
+
+import Foundation
+
+protocol HTTPClientContract {
+    func get(url: String)
+}
