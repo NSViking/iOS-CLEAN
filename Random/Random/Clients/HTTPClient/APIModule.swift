f7f2ae55bab435f579edcbdb43f836a9b753fbd2
NSViking <victorvicente19@gmail.com>
2018-10-20 14:08:25 +0200

    Added APIModule for network connections

---
+++ Random/Random/Clients/HTTPClient/APIModule.swift
@@ -0,0 +1,67 @@
+//
+//  APIModule.swift
+//  Random
+//
+//  Created by Víctor Vicente on 20/10/2018.
+//  Copyright © 2018 AKA7. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+public enum APIModule {
+    case get(results: Int)
+}
+
+extension APIModule {
+    
+    static func getProvider(baseurl: String) -> MoyaProvider<APIModule> {
+        
+        let apiProvider = MoyaProvider<APIModule>(endpointClosure: {(target: APIModule) -> Moya.Endpoint in
+            
+            let base = URL(string: "http://api.randomuser.me")
+            
+            let url = base?.appendingPathComponent(target.path).absoluteString
+            let endpoint: Moya.Endpoint = Endpoint(url: url!, sampleResponseClosure: { .networkResponse(200, target.sampleData) }, method: target.method, task: target.task, httpHeaderFields: target.headers)
+            
+            return endpoint
+        })
+        
+        return apiProvider
+    }
+}
+
+extension APIModule: Moya.TargetType {
+    public var baseURL: URL {
+        return URL(string: "https://not.in.use/")!
+    }
+    
+    public var path: String {
+        switch self {
+        case .get(results: _):
+            return "/"
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        case .get(results: _):
+            return .get
+        }
+    }
+    
+    public var sampleData: Data {
+        return "".data(using: String.Encoding.utf8)!
+    }
+    
+    public var task: Task {
+        switch self {
+        case .get(let results):
+            return .requestParameters(parameters: ["results": results], encoding: String.Encoding.utf8 as! ParameterEncoding)
+        }
+    }
+    
+    public var headers: [String : String]? {
+        return nil
+    }
+}
