3314ebacf1f5b3c702498d1460b294ea2c35988e
NSViking <victorvicente19@gmail.com>
2018-10-20 12:49:14 +0200

    Create project

---
+++ Random/Random/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random
+//
+//  Created by Víctor Vicente on 20/10/2018.
+//  Copyright © 2018 AKA7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
