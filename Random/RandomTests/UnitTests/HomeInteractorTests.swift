ea93435b3c59ba592fefb727c48f55a61e62ef2e
NSViking <victorvicente19@gmail.com>
2018-10-21 15:48:30 +0200

    Added HomeInteractorTests

---
+++ Random/RandomTests/UnitTests/HomeInteractorTests.swift
@@ -0,0 +1,23 @@
+//
+//  HomeInteractorTests.swift
+//  RandomTests
+//
+//  Created by Víctor Vicente on 21/10/2018.
+//  Copyright © 2018 AKA7. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Moya
+import RxSwift
+import RxBlocking
+import Mockit
+
+@testable import Random
+
+class HomeInteractorTests: XCTestCase {
+	
+	func testGetUsers() {
+
+	}
+}
