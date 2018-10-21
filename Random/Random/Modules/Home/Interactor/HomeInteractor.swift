0a088e1906dbdd9782f5ec04e1a98769912fe1e0
NSViking <victorvicente19@gmail.com>
2018-10-21 15:41:26 +0200

    Created HomeInteractor
    Here we will add methods to communicate between presentation layers and data layers, for example, remove duplicate elements is a “bussines rule” so it is the place to do this.
    
    Interactor, the place where we put the bussines rules

---
+++ Random/Random/Modules/Home/Interactor/HomeInteractor.swift
@@ -0,0 +1,17 @@
+//
+//  HomeInteractor.swift
+//  Random
+//
+//  Created by Víctor Vicente on 21/10/2018.
+//  Copyright © 2018 AKA7. All rights reserved.
+//
+
+import Foundation
+
+class HomeInteractor {
+	var repo: UserRepositoryContract
+	
+	init(repo: UserRepositoryContract) {
+		self.repo = repo
+	}
+}
