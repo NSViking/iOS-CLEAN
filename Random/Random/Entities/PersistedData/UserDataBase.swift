93cd2f1e83c15774de9094c9e60f15b390e2fbb9
NSViking <victorvicente19@gmail.com>
2018-10-22 02:05:53 +0200

    Added realm to persist data

---
+++ Random/Random/Entities/PersistedData/UserDataBase.swift
@@ -0,0 +1,22 @@
+//
+//  UserDataBase.swift
+//  Random
+//
+//  Created by Víctor Vicente on 22/10/2018.
+//  Copyright © 2018 AKA7. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class UserDataBase: Object {
+    @objc dynamic var title = ""
+    @objc dynamic var first = ""
+    @objc dynamic var last = ""
+    @objc dynamic var email = ""
+    @objc dynamic var phone = ""
+    @objc dynamic var gender = ""
+    @objc dynamic var picture = ""
+    @objc dynamic var registered = ""
+    @objc dynamic var location: LocationDataBase? = nil
+}
